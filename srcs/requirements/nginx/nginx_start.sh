79e386b8d93c165d4118a14cfd206bbc0db06f5b
zico15 <zequielzico@gmail.com>
2023-05-04 21:42:47 +0100

    cls

--- srcs/requirements/nginx/nginx_start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Create a symbolic link to the SSL certificate and key files
-ln -s /etc/nginx/ssl/nginx.crt /etc/ssl/certs/nginx.crt
-ln -s /etc/nginx/ssl/nginx.key /etc/ssl/private/nginx.key
-
-# Start Nginx in the foreground
-nginx -g "daemon off;"
